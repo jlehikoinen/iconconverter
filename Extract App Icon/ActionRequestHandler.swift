5ec65c01817d395ccdc40f84803c0a10e1d9a2a8
Janne Lehikoinen <janne@lehi.fi>
2025-11-03 11:28:22 +0200

    Fix extension lifecycle

--- Extract App Icon/ActionRequestHandler.swift
+++ Extract App Icon/ActionRequestHandler.swift
@@ -19,15 +19,29 @@ class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
         guard let inputAttachments = inputItem.attachments else {
             preconditionFailure("Expected a valid array of attachments")
         }
-
+        
         for attachment in inputAttachments {
-            attachment.loadInPlaceFileRepresentation(forTypeIdentifier: "public.directory") { [unowned self] (appUrl, inPlace, error) in
+            
+            // Allow only app bundle attachment types
+            guard let attachmentTypeID = attachment.registeredTypeIdentifiers.first else { continue }
+            Logger.quickActionExtension.info("Attachment type: \(attachmentTypeID)")
+            if attachmentTypeID != AppConfig.appBundleAttachmentType {
+                // cancelRequest crashes the extension that's why "success" = completeRequest is used here
+                context.completeRequest(returningItems: [inputItem], completionHandler: nil)
+//                context.cancelRequest(withError: NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil))
+                return
+            }
+            
+            attachment.loadInPlaceFileRepresentation(forTypeIdentifier: AppConfig.appBundleAttachmentType) { (appUrl, inPlace, error) in
                 if let appUrl {
                     let urlSchemeTargetUrl = appUrl.path
                     Logger.quickActionExtension.info("Finder Quick Action URL: \(urlSchemeTargetUrl)")
                     
                     /// Custom URL Scheme is configured in Info > URL Types
                     NSWorkspace.shared.open(URL(string: "iconconverter:\(urlSchemeTargetUrl)")!)
+                    
+                    // Notify the action is done with success
+                    context.completeRequest(returningItems: [inputItem], completionHandler: nil)
                 }
             }
         }
--- Shared/00_App/AppConfig.swift
+++ Shared/00_App/AppConfig.swift
@@ -16,6 +16,9 @@ enum AppConfig {
     static let appBundleExtension = "app"
     static let pngExtension = "png"
     
+    // Finder extension
+    static let appBundleAttachmentType = "com.apple.application-bundle"
+    
     // Build Settings > Versioning > Marketing Version = CFBundleShortVersionString in Info.plist
     static let appShortVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
     static let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
